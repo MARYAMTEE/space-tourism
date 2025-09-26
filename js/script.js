227ebaadd7511f692111b9df2a6d8700b6af8451
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-09-26 13:18:25 +0100

    update technology page

--- js/script.js
+++ js/script.js
@@ -171,3 +171,65 @@ crewBtn.forEach((b, index) => {
     })
   })
 });
+
+// Technology page
+const technology = [
+  {
+    heading: "Launch vehicle",
+    image: {
+      png: "./assets/technology/image-launch-vehicle-landscape.jpg",
+    },
+    description: `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a 
+  payload from Earth's surface to space, usually to Earth orbit or beyond. Our 
+  WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, 
+  it's quite an awe-inspiring sight on the launch pad!`
+  },
+  {
+    heading: "Spaceport",
+    image: {
+      png: "./assets/technology/image-spaceport-landscape.jpg",
+    },
+    description: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, 
+  by analogy to the seaport for ships or airport for aircraft. Based in the 
+  famous Cape Canaveral, our spaceport is ideally situated to take advantage 
+  of the Earth’s rotation for launch.`
+  },
+  {
+    heading: "Space capsule",
+    image: {
+      png: "./assets/technology/image-space-capsule-landscape.jpg",
+    },
+    description: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry 
+  capsule to reenter the Earth's atmosphere without wings. Our capsule is where 
+  you'll spend your time during the flight. It includes a space gym, cinema, 
+  and plenty of other activities to keep you entertained.`
+  }
+];
+
+const techBtn = document.querySelectorAll(".tech__tabs [role='tab']");
+const techImg = document.querySelector(".tech__img");
+const techHeading = document.querySelector(".tech__heading");
+const techDescription = document.querySelector(".tech__description");
+
+techBtn.forEach((t, index) => {
+  t.addEventListener("click", () => {
+    techBtn.forEach(btn => btn.setAttribute("aria-selected", "false"));
+    t.setAttribute("aria-selected", "true");
+
+    const getTech = technology[index];
+
+    // update DOM
+    if (techImg) {
+      techImg.src = getTech.image.png;
+      techImg.alt = getTech.heading;
+      techHeading.textContent = getTech.heading;
+      techDescription.textContent = getTech.description;
+    }
+
+    [techImg, techHeading, techDescription].forEach(le => {
+      le.classList.remove("fade-in");
+      void le.offsetWidth;
+      le.classList.add("fade-in");
+    })
+  })
+});
